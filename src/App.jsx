50ddf01dbd2c33915e4fa0ecc8751b219fc2d139
Alexander Hilliard <TheEnvx@gmail.com>
2025-04-03 19:17:09 -0600

    added database handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
+import Toolbar from './components/toolbar'
 import './App.css'
+import getAllData from './modules/data'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [artists, setArtists] = useState([])
+  const [galleries, setGalleries] = useState([])
+  const [genres, setGenres] = useState([])
+  const [paintings, setPaintings] = useState([])
+  const [oading, setLoading] = useState(false)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  //data fetching
+  localStorage.clear(); //testing
+  useEffect(()=>{
+    //get all data, passing all setters labeled in an obj, and data loading setter
+    getAllData({artists:setArtists,
+      galleries:setGalleries,
+      genres:setGenres,
+      paintings:setPaintings,
+      loading:setLoading})
+  }, []);
+
+  return(
+    <main>
+      <Toolbar />
+    </main>
   )
+
 }
 
 export default App
---
+++ src/modules/data.jsx
@@ -0,0 +1,82 @@
+import { createClient } from "@supabase/supabase-js"
+
+//NEED TO BE PUT INTO ENV VARIABLES
+const supaUrl = 'https://ajyrcmphdwzxigpkrkth.supabase.co';
+const supaAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFqeXJjbXBoZHd6eGlncGtya3RoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDAwOTIyNDgsImV4cCI6MjA1NTY2ODI0OH0.lVH3PuXDObLiNT2-u73My5I4y_COMPRHX1gvlGPmKLY';
+
+const db = createClient(supaUrl, supaAnonKey);
+
+async function getAllData(setters){
+    setters.loading(true)
+    console.log(`data loading`)
+    getArtists(setters.artists)
+    getGalleries(setters.galleries,setters.loading)
+    getGenres(setters.genres)
+    getPaintings(setters.paintings)
+    console.log(`data loaded`)
+    setters.loading(false)
+}
+
+//fetch functions - if data not in local storage
+async function fetchArtists(setter) {
+    const { data } = await db.from("artists").select('*');
+    setter(data);
+    localStorage.artists = data
+  }
+
+//galleries include loading state setter, as is the first screen shown on the app
+async function fetchGalleries(setter,loading) {
+    loading(true)
+    const { data } = await db.from("galleries").select('*');
+    setter(data);
+    loading(false)
+    localStorage.Galleries = data
+  }
+
+async function fetchPaintings(setter) {
+    const { data } = await db.from("paintings").select('*');
+    setter(data);
+    localStorage.paintings = data
+  }
+
+async function fetchGenres(setter) {
+    const { data } = await db.from("genres").select('*');
+    setter(data);
+    localStorage.paintings = data
+  }
+
+//get functions - checks local storage first, otherwise calls fetch function
+function getArtists(setter){
+    if (localStorage.artists){
+        setter(localStorage.artists)
+    } else {
+        fetchArtists(setter)
+    }
+}
+
+//includes loading setter as is first screen seen on the app
+function getGalleries(setter,loading){
+    if (localStorage.galleries){
+        setter(localStorage.galleries)
+    } else {
+        fetchGalleries(setter,loading)
+    }
+}
+
+function getPaintings(setter){
+    if (localStorage.paintings){
+        setter(localStorage.paintings)
+    } else {
+        fetchPaintings(setter)
+    }
+}
+
+function getGenres(setter){
+    if (localStorage.genres){
+        setter(localStorage.genres)
+    } else {
+        fetchGenres(setter)
+    }
+}
+
+export default getAllData;
